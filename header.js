488d8c41d3c9aa1d2b22092f28770d3c35f0a567
buthikimanhat <buthikimanhvtath5@gmail.com>
2019-11-07 23:40:02 +0700

    add thietlapkpi and update template

--- header.js
+++ header.js
@@ -1,42 +1,8 @@
-document.write('<nav class="navbar navbar-expand-lg navbar-light bg-light">\n');
-document.write('          <div class="container-fluid">\n');
-document.write(
-  '            <button type="button" id="sidebarCollapse" class="btn btn-info">\n'
-);
-document.write('              <i class="fas fa-align-left"></i>\n');
-document.write("              <span>Toggle Sidebar</span>\n");
-document.write("            </button>\n");
-document.write("            <button\n");
-document.write(
-  '              class="btn btn-dark d-inline-block d-lg-none ml-auto"\n'
-);
-document.write('              type="button"\n');
-document.write('              data-toggle="collapse"\n');
-document.write('              data-target="#navbarSupportedContent"\n');
-document.write('              aria-controls="navbarSupportedContent"\n');
-document.write('              aria-expanded="false"\n');
-document.write('              aria-label="Toggle navigation"\n');
-document.write("            >\n");
-document.write('              <i class="fas fa-align-justify"></i>\n');
-document.write("            </button>\n");
-document.write("\n");
-document.write(
-  '            <div class="collapse navbar-collapse" id="navbarSupportedContent">\n'
-);
-document.write('              <ul class="nav navbar-nav ml-auto">\n');
-document.write('                <li class="nav-item active">\n');
-document.write('                  <a class="nav-link" href="#">Page</a>\n');
-document.write("                </li>\n");
-document.write('                <li class="nav-item">\n');
-document.write('                  <a class="nav-link" href="#">Page</a>\n');
-document.write("                </li>\n");
-document.write('                <li class="nav-item">\n');
-document.write('                  <a class="nav-link" href="#">Page</a>\n');
-document.write("                </li>\n");
-document.write('                <li class="nav-item">\n');
-document.write('                  <a class="nav-link" href="#">Page</a>\n');
-document.write("                </li>\n");
-document.write("              </ul>\n");
-document.write("            </div>\n");
-document.write("          </div>\n");
-document.write("        </nav>");
+document.write('    <nav class="navbar navbar-expand-lg navbar-light bg-light">\n');
+document.write('      <div class="container-fluid">\n');
+document.write( '       <div class=\"form-group has-search\">\n' );
+document.write( '         <span class=\"fa fa-search form-control-feedback\"></span>\n' );
+document.write( '         <input type=\"text\" class=\"form-control\" placeholder=\"Search\">\n' );
+document.write( '       </div>' );
+document.write("       </div>\n");
+document.write("      </nav>");
